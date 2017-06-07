38077be2a4fa0e9d94bea29985265cfc01868572
helen <ellenamhm@gmail.com>
2017-06-07 22:32:18 +0300

    first

---
+++ app/js/init.js
@@ -0,0 +1,38 @@
+    $(document).ready(function() {
+
+       $('#menu').flexMenu();
+ 
+  var slickParams = {
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      speed: 300,
+      dots: false,
+      arrows:true,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  };
+
+
+});
